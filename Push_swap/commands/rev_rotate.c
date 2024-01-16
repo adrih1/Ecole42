649ba14e4833ae9d2e868424079b001f1c24a429
Adrien Hors <ahors@bess-f4r6s1.clusters.42paris.fr>
2024-01-16 14:36:55 +0100

    Norminette Done bis

--- Push_swap/commands/rev_rotate.c
+++ Push_swap/commands/rev_rotate.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_rotate.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahors <ahors@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 14:33:33 by ahors             #+#    #+#             */
+/*   Updated: 2024/01/16 14:33:42 by ahors            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../header/push_swap.h"
 
 static void	rev_rotate(t_stack_node **stack)
@@ -7,28 +19,28 @@ static void	rev_rotate(t_stack_node **stack)
 	if (!*stack || !(*stack)->next)
 		return ;
 	last = ft_find_last_node(*stack);
-	last->prev->next = NULL; //Assign to the `next` attribute of the node before itself, `NULL` effectively setting it as the current last node
-	last->next = *stack; //Assign to its own `next` attribute as the top node of the stack
-	last->prev = NULL; //Detach itself from the node before i
-	*stack = last;  //Complete appending itself to the top of the stack, and now holds the pointer to the top node
-	last->next->prev = last; //Update the current last node of the stack
+	last->prev->next = NULL;
+	last->next = *stack;
+	last->prev = NULL;
+	*stack = last;
+	last->next->prev = last;
 }
 
-void	rra(t_stack_node **a, bool print) //Rotate the bottom of `a` to the top of the stack and print the instruction
+void	rra(t_stack_node **a, bool print)
 {
 	rev_rotate(a);
 	if (!print)
 		write(1, "rra\n", 4);
 }
 
-void	rrb(t_stack_node **b, bool print) //Rotate the bottom of `b` to the top of the stack and print the instruction
+void	rrb(t_stack_node **b, bool print)
 {
 	rev_rotate(b);
 	if (!print)
 		write(1, "rrb\n", 4);
 }
 
-void	rrr(t_stack_node **a, t_stack_node **b, bool print) //Stimultaneously rotate both stacks' bottom nodes to the top of their stacks, halving the cost of instructions
+void	rrr(t_stack_node **a, t_stack_node **b, bool print)
 {
 	rev_rotate(a);
 	rev_rotate(b);
