e43cf3045992e6433aeb5e1b9cdffaf369736b14
Adrien Hors <ahors@paul-f4Br3s6.clusters.42paris.fr>
2024-02-15 17:42:46 +0100

    Structure for both executables done

--- Minitalk/srcs/client.c
+++ Minitalk/srcs/client.c
@@ -6,7 +6,47 @@
 /*   By: ahors <ahors@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 16:57:46 by ahors             #+#    #+#             */
-/*   Updated: 2024/02/14 17:06:27 by ahors            ###   ########.fr       */
+/*   Updated: 2024/02/15 17:37:08 by ahors            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../header/minitalk.h"
+
+void	ft_chartobits(char c)
+{
+	int	i;
+	int	bit;
+
+	i = 7;
+	while (i >= 0)
+	{
+		bit = (c >> i) & 1;
+		printf("%d", bit);
+		i--;
+	}
+	printf("\n");
+}
+
+void	ft_stringtobits(int pid, char *str)
+{
+	int	i;
+
+	i = 0;
+    printf("PID: %d\n", pid);
+	while (str[i])
+	{
+        
+		ft_chartobits(str[i]);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+    if (ac == 2)
+	    ft_stringtobits(0, av[1]);
+    else
+        printf("No string provided\n");
+
+    return (0);
+}
--- Minitalk/srcs/server.c
+++ Minitalk/srcs/server.c
@@ -6,15 +6,24 @@
 /*   By: ahors <ahors@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 16:57:51 by ahors             #+#    #+#             */
-/*   Updated: 2024/02/14 17:20:42 by ahors            ###   ########.fr       */
+/*   Updated: 2024/02/15 17:39:06 by ahors            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minitalk.h"
 
-
+void	handler(void)
+{
+	write(STDOUT_FILENO, "Hello\n", 6);
+}
 
 int	main(void)
 {
-	ft_printf("L'ID de la client est: %d\n", getpid());
+	// signal(SIGINT, handler);
+	while(1)
+	{
+		ft_printf("L'ID du client est: %d\n", getpid());
+		sleep(1);	
+	}
+	return(0);
 }
