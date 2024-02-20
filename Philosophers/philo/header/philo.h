f81050a566b2e184cf2e501c67fd0cb17d776148
Adrien Hors <ahors@bess-f2r3s1.clusters.42paris.fr>
2024-02-20 18:05:32 +0100

    Base of structures

---
+++ Philosophers/philo/header/philo.h
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahors <ahors@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 13:58:14 by ahors             #+#    #+#             */
+/*   Updated: 2024/02/20 16:12:06 by ahors            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+/*
+---------------------------------------------------------------------------------
+|                                    LIBRAIRIE                                  |
+---------------------------------------------------------------------------------
+*/
+
+# include <pthread.h>
+
+/*
+---------------------------------------------------------------------------------
+|                                    FONCTIONS                                  |
+---------------------------------------------------------------------------------
+*/
+
+
+typedef struct s_philosopher
+{
+	pthread_t		thread;
+	int				id;
+	int				eating;
+	// int				status; Pas sur d'en avoir besoin
+	int				meals_eaten;
+	size_t			last_meal;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	size_t			start_time;
+	int				num_of_philos;
+	int				num_times_to_eat;
+	int				*dead;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*write_lock;
+	pthread_mutex_t	*dead_lock;
+	pthread_mutex_t	*meal_lock;
+}					t_philosopher;
+
+typedef struct s_program
+{
+	int				dead_flag;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	meal_lock;
+	pthread_mutex_t	write_lock;
+	t_philosopher	*philos;
+}					t_program;
+
+#endif
