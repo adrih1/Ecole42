bc5f3a3b5da6af4543d6f17a841c1e7606126ac2
Adrien Hors <adrienhors1@gmail.com>
2024-11-29 11:45:51 +0100

    CPP04 - ex00 - changed main

--- CPP/cpp04/ex00/main.cpp
+++ CPP/cpp04/ex00/main.cpp
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-#include "Animal.hpp"
-#include "Dog.hpp"
-#include "Cat.hpp"
-=======
 #include "includes/Dog.hpp"
 #include "includes/Cat.hpp"
 #include "includes/WrongCat.hpp"
@@ -20,22 +15,12 @@
 //     delete j;
 //     return 0;
 // }
->>>>>>> 412540f22a7ac7f3ee42edc048d78652fad56b73
 
 int main()
 {
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
     const Animal* i = new Cat();
-<<<<<<< HEAD
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); //will output the cat sound!
-    j->makeSound();
-    meta->makeSound();
-
-return 0;
-=======
 
     std::cout << j->getType() << " " << std::endl; 
     std::cout << i->getType() << " " << std::endl; 
@@ -58,5 +43,4 @@ return 0;
     delete metaWrong;
     delete k;
     return (0);
->>>>>>> 412540f22a7ac7f3ee42edc048d78652fad56b73
 }
