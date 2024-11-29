cab0e0a0d70c27f241c44785b2a0aa912b5f1864
Adrien Hors <adrienhors1@gmail.com>
2024-11-29 14:00:58 +0100

    CPP04 -ex01 : Slightly improved main

--- CPP/cpp04/ex01/main.cpp
+++ CPP/cpp04/ex01/main.cpp
@@ -3,12 +3,18 @@
 #include "./includes/Cat.hpp"
 
 
+#define RESET "\033[0m"
+#define GREEN "\033[32m"
+#define CYAN "\033[36m"
+#define YELLOW "\033[33m"
+#define BOLD_CYAN "\033[1;36m"
+
 int main ()
 {
 	const int NUM_ANIMALS = 100;
     Animal *animals[NUM_ANIMALS];
 
-	std::cout << "Creating Animals" << std::endl;
+	std::cout << BOLD_CYAN << "Creating Animals" << RESET << std::endl;
 	for (int i = 0; i < NUM_ANIMALS; i++)
 	{
 		if(i % 2 == 0)
@@ -17,13 +23,13 @@ int main ()
 			animals[i] = new Cat();
 	}
 
-	std::cout << "\nAnimals making sound" << std::endl;
+	std::cout << GREEN << "\nAnimals making sound" << RESET << std::endl;
 	for (int i = 0; i < NUM_ANIMALS; i++)
 		animals[i]->makeSound();
 	
-	std::cout << "\nDeleting Animals" << std::endl;
+	std::cout << GREEN << "\nDeleting Animals" << RESET << std::endl;
 	for (int i = 0; i < NUM_ANIMALS; i++)
 		delete animals[i];
-
+	std::cout << BOLD_CYAN << "End of test" << RESET << std::endl;
 	return (0);
 }
