c91b26a05a6c9b401b9a7de295fa09b636292b47
Adrien Hors <ahors@made-f0Ar12s12.clusters.42paris.fr>
2025-01-07 15:45:43 +0100

    cpp08 - ex02

---
+++ CPP/cpp08/ex02/main.cpp
@@ -0,0 +1,33 @@
+#include "MutantStack.hpp"
+#include <iostream>
+#include <vector>
+
+int main() {
+    // Utilisation avec le conteneur par défaut (std::deque)
+    MutantStack<int> mstack;
+
+    mstack.push(10);
+    mstack.push(20);
+    mstack.push(30);
+
+    std::cout << "Iterating through MutantStack (default deque):" << std::endl;
+    for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    // Utilisation avec un std::vector comme conteneur
+    MutantStack<int, std::vector<int> > mstackVec;
+
+    mstackVec.push(100);
+    mstackVec.push(200);
+    mstackVec.push(300);
+
+    std::cout << "Iterating through MutantStack (vector):" << std::endl;
+    for (MutantStack<int, std::vector<int> >::iterator it = mstackVec.begin(); it != mstackVec.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
