f8eab0ed0d44615dd119174b18fa9b73129213ce
Adrien Hors <ahors@paul-f3Br8s1.clusters.42paris.fr>
2025-01-21 07:41:09 +0100

    CPP05-ex02: Better main

--- CPP/cpp05/ex02/main.cpp
+++ CPP/cpp05/ex02/main.cpp
@@ -6,12 +6,15 @@
 
 #define RESET "\033[0m"
 #define BLUE "\033[34m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define ORANGE "\033[38;5;208m"
 #define YELLOW "\033[33m"
 
 
 void testPresidentialPardonForm() {
-    std::cout << BLUE << "Test: PresidentialPardonForm Form" << RESET << std::endl;
-    std::cout << YELLOW << "Valid Test" << RESET << std::endl;
+    std::cout << GREEN << "Test: PresidentialPardonForm Form" << RESET << std::endl;
+    std::cout << ORANGE << "Valid Test" << RESET << std::endl;
     try {
         Bureaucrat Gustavo("Gustavo", 1);
         PresidentialPardonForm f1("Target 1");
@@ -23,7 +26,7 @@ void testPresidentialPardonForm() {
     } catch (const std::exception & e) {
         std::cerr << e.what() << std::endl;
     }
-    std::cout << YELLOW << "Error Test" << RESET << std::endl;
+    std::cout << ORANGE << "Error Test" << RESET << std::endl;
     try {
         Bureaucrat Gustavo("Gustavo", 30);
         PresidentialPardonForm f1("Target 1");
@@ -38,8 +41,8 @@ void testPresidentialPardonForm() {
 }
 
 void testRobotomyPardonForm() {
-    std::cout << BLUE << "\nTest : Robotomy Form" << RESET << std::endl;
-    std::cout << YELLOW << "Valid Test" << RESET << std::endl;
+    std::cout << GREEN << "\nTest : Robotomy Form" << RESET << std::endl;
+    std::cout << ORANGE << "Valid Test" << RESET << std::endl;
     try {
         Bureaucrat Gustavo("Gustavo", 1);
         RobotomyRequestForm f1("Target 1");
@@ -51,7 +54,7 @@ void testRobotomyPardonForm() {
     } catch (const std::exception & e) {
         std::cerr << e.what() << std::endl;
     }
-    std::cout << YELLOW << "Error Test" << RESET << std::endl;
+    std::cout << ORANGE << "Error Test" << RESET << std::endl;
     try {
         Bureaucrat Gustavo("Gustavo", 73);
         RobotomyRequestForm f1("Target 1");
@@ -66,8 +69,8 @@ void testRobotomyPardonForm() {
 
 
 void testShruberryPardonForm() {
-    std::cout << BLUE << "\nTest : Shruberry Form" << RESET << std::endl;
-    std::cout << YELLOW << "Valid Test" << RESET << std::endl;
+    std::cout << GREEN << "\nTest : Shruberry Form" << RESET << std::endl;
+    std::cout << ORANGE << "Valid Test" << RESET << std::endl;
     try {
         Bureaucrat Gustavo("Gustavo", 1);
         ShrubberyCreationForm f1("Target 1");
@@ -79,7 +82,7 @@ void testShruberryPardonForm() {
     } catch (const std::exception & e) {
         std::cerr << e.what() << std::endl;
     }
-    std::cout << YELLOW << "Error Test" << RESET << std::endl;
+    std::cout << ORANGE << "Error Test" << RESET << std::endl;
      try {
         Bureaucrat Gustavo("Gustavo", 146);
         ShrubberyCreationForm f1("Target 1");
