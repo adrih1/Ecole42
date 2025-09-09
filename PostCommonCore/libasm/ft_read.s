641e1a71a214b0c60baf6ec6b70db0828ef6c3fb
Adrien Hors <ahors@f6r9s8.clusters.42paris.fr>
2025-09-09 12:50:02 +0200

    Libasm - Handled errno return in read and write

--- PostCommonCore/libasm/ft_read.s
+++ PostCommonCore/libasm/ft_read.s
@@ -1,7 +1,20 @@
 global ft_read
+extern __errno_location 
 
 ft_read:
     ; RDI = fd, RSI = buffer, RDX = count
-    mov rax, 1 ; syscall is 1 for write in 64 bits
+    mov rax, 0; syscall is 0 for read in 64 bits
     syscall
+    cmp rax, 0
+    jl error
+    ret
+
+error:
+    neg rax                 ; RAX = erno positif
+    mov rdi, rax            ; rdi serves as a bufer because rax will take the return of erno location
+    sub rsp, 8              ; Aligning the stack
+    call __errno_location   ; returns a pointer to erno
+    add rsp, 8                 
+    mov [rax], rdi          ; rax holds the adres of erno so we are putting rdi in erno
+    mov rax, -1             ; putting the correct value oif -1 in a write return
     ret
