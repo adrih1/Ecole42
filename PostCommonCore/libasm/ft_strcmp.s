39c598a2f5771ccc477acc3140205be8020de673
Adrien Hors <ahors@f6r9s8.clusters.42paris.fr>
2025-09-09 13:53:49 +0200

    Libasm - Compiles + First tests done

--- PostCommonCore/libasm/ft_strcmp.s
+++ PostCommonCore/libasm/ft_strcmp.s
@@ -3,24 +3,25 @@ global ft_strcmp
 ft_strcmp:
     ; RAX return value
     ; RDI = 1st argument , RSI = 2e argmuent
-.loop
+
+.loop:
     mov al, BYTE[rdi]
     mov bl, BYTE[rsi]
     cmp al, bl
     jne .diff ; if different calculate difference
     cmp al, 0 ; check if end of string
-    je equal ; si "\0" s1 == s2
+    je .equal ; si "\0" s1 == s2
     inc rdi
     inc rsi
     jmp .loop
 
 
-.diff
+.diff:
     movsx rax, al ; extend char into signed integer 64 bits
     movsx rbx, bl ; extend char into signed integer 64 bits
     sub rax, rbx
     ret
 
-.equal
+.equal:
     xor rax,rax
     ret
