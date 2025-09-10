335d8e0f7aeec12637ea12213330969d983ea17b
Adrien Hors <ahors@f4r10s3.clusters.42paris.fr>
2025-09-10 11:09:28 +0200

    Libasm - More comments for clarity

--- PostCommonCore/libasm/ft_read.s
+++ PostCommonCore/libasm/ft_read.s
@@ -4,13 +4,13 @@ extern __errno_location
 ft_read:
     ; RDI = fd, RSI = buffer, RDX = count
     mov rax, 0; syscall is 0 for read in 64 bits
-    syscall
+    syscall ;' if syscall succeeds rax will hold the number of bytes read, if not it will hold -erno
     cmp rax, 0
-    jl error
+    jl error ; jump if less than 0 
     ret
 
 error:
-    neg rax                 ; RAX = erno positif
+    neg rax                 ; transforming negative rax (-erno) value into positive
     mov rdi, rax            ; rdi serves as a bufer because rax will take the return of erno location
     call __errno_location   ; returns a pointer to erno
     mov [rax], rdi          ; rax holds the adres of erno so we are putting rdi in erno
