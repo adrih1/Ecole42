8062b676d14285ab7fe5f058f28728524fc1caae
Adrien Hors <adrienhors1@gmail.com>
2025-09-29 08:39:47 +0200

    Libasm-ft_strlen

--- PostCommonCore/libasm/ft_strlen.s
+++ PostCommonCore/libasm/ft_strlen.s
@@ -1,31 +1,16 @@
-bits 64
-default rel
+global ft_strlen
 
-SECTION .data
-    string_test db "salut", 0xA, 0
+ft_strlen:
+    mov rax, 0  ; init rax to 0
+    jmp loop_count ; jump to loop_countfunction
 
-SECTION .text
-    global main
-    global strlen
-    extern printf
+loop_count:
+    cmp BYTE [rdi + rax], 0 ;
+    je  exit                ; je = rdi[rax] == 0- -> exit
+    inc rax                 ; increment 
+    jmp loop_count
 
-strlen:
-    push rbp
-    mov rbp, rsp
+exit: 
+    ret                     ; return rax
 
-    mov rdx, 
 
-main:
-    ; prologue
-    push rbp
-    mov rbp, rsp
-
-    ; appel de printf("Hello World!\n");
-    lea rdi, [rel msg]   ; 1er argument (format string)
-    xor eax, eax         ; printf variadique => RAX doit être 0
-    call strlen
-
-    ; appel de exit(0) via syscall
-    mov rax, 60          ; numéro du syscall: exit
-    xor rdi, rdi         ; code retour = 0
-    syscall
