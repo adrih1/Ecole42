2c2347e854fbd746f840ef7704f99119fa31544c
Adrien Hors <ahors@f6r2s1.clusters.42paris.fr>
2026-02-25 13:48:36 +0100

    Added directory for all kfs projects

---
+++ PostCommonCore/kfs/kfs1/includes/terminal.h
@@ -0,0 +1,47 @@
+#ifndef TERMINAL_H
+# define TERMINAL_H
+
+# include <stddef.h>
+# include <stdint.h>
+# include <stdarg.h>
+
+/* --- VGA CONFIG --- */
+#define VGA_WIDTH  80
+#define VGA_HEIGHT 25
+#define VGA_BUF    0xB8000
+
+
+enum vga_color {
+    VGA_COLOR_BLACK = 0,
+    VGA_COLOR_BLUE = 1,
+    VGA_COLOR_GREEN = 2,
+    VGA_COLOR_CYAN = 3,
+    VGA_COLOR_RED = 4,
+    VGA_COLOR_MAGENTA = 5,
+    VGA_COLOR_BROWN = 6,
+    VGA_COLOR_LIGHT_GREY = 7,
+    VGA_COLOR_DARK_GREY = 8,
+    VGA_COLOR_LIGHT_BLUE = 9,
+    VGA_COLOR_LIGHT_GREEN = 10,
+    VGA_COLOR_LIGHT_CYAN = 11,
+    VGA_COLOR_LIGHT_RED = 12,
+    VGA_COLOR_LIGHT_MAGENTA = 13,
+    VGA_COLOR_LIGHT_BROWN = 14,
+    VGA_COLOR_WHITE = 15,
+};
+
+/* Scancode -> ASCII (QWERTY) */
+extern const unsigned char g_kbd_qwerty[128];
+
+void terminal_initialize(void);
+void terminal_putchar(char c);
+void terminal_putstr(const char* data);
+void terminal_putnbr_base(uint32_t n, int base);
+void terminal_setcolor(enum vga_color fg, enum vga_color bg);
+void printk(const char *format, ...);
+size_t strlen(const char* str);
+extern void outb(uint16_t port, uint8_t data);
+extern uint8_t inb(uint8_t data);
+
+
+#endif
---
+++ PostCommonCore/kfs/kfs1/src/terminal.c
@@ -0,0 +1,152 @@
+#include "terminal.h"
+#include <stdarg.h>
+
+/* State of terminal */
+static size_t   g_x = 0;
+static size_t   g_y = 0;
+static uint8_t  g_color;
+
+
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
+    return fg | (bg << 4);
+}
+
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
+    return (uint16_t) uc | (uint16_t) color << 8;
+}
+
+/**
+ * Defines current display color
+ */
+void terminal_setcolor(enum vga_color fg, enum vga_color bg) {
+    g_color = vga_entry_color(fg, bg);
+}
+
+
+/**
+ * Communicates with the CRT controller to move the blinking cursor.
+ * Uses I/O ports 0x3D4 (index) and 0x3D5 (data).
+ */
+void terminal_update_cursor(void) {
+    uint16_t pos = g_y * VGA_WIDTH + g_x;
+
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (uint8_t)(pos & 0xFF));
+    
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
+}
+
+/**
+ * Moves all screen 1 row up if we go beyond height.
+ * Cleans last line with empty spaces.
+ */
+static void terminal_scroll(void) {
+    uint16_t *buffer = (uint16_t*)VGA_BUF;
+
+    // Move line 0 to 24 up, deletes line 0
+    for (size_t i = 0; i < (VGA_HEIGHT - 1) * VGA_WIDTH; i++) {
+        buffer[i] = buffer[i + VGA_WIDTH];
+    }
+
+    // Empty last line (line 24)
+    for (size_t i = (VGA_HEIGHT - 1) * VGA_WIDTH; i < VGA_HEIGHT * VGA_WIDTH; i++) {
+        buffer[i] = vga_entry(' ', g_color);
+    }
+}
+
+/**
+ * Initialize screen: empty VGA memory and reset cursor.
+ */
+void terminal_initialize(void) {
+    g_x = 0;
+    g_y = 0;
+    terminal_setcolor(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+    
+    uint16_t *buffer = (uint16_t*)VGA_BUF;
+    for (size_t i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
+        buffer[i] = vga_entry(' ', g_color);
+    }
+    terminal_update_cursor();
+}
+
+
+/**
+ * Display one char (handle '\n' and overlay)
+ */
+void terminal_putchar(char c) {
+    uint16_t *buffer = (uint16_t*)VGA_BUF;
+
+    if (c == '\n') {
+        g_x = 0;
+        g_y++;
+    } else {
+        buffer[g_y * VGA_WIDTH + g_x] = vga_entry(c, g_color);
+        if (++g_x == VGA_WIDTH) {
+            g_x = 0;
+            g_y++;
+        }
+    }
+
+    // Handling screen overlay
+    if (g_y >= VGA_HEIGHT) {
+        terminal_scroll();
+        g_y = VGA_HEIGHT - 1;
+    }
+
+    terminal_update_cursor();
+}
+
+void terminal_putstr(const char* data) {
+    for (size_t i = 0; data[i] != '\0'; i++) {
+        terminal_putchar(data[i]);
+    }
+}
+
+/**
+ * Fromat and display complex messages.
+ */
+void printk(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    for (size_t i = 0; format[i] != '\0'; i++) {
+        if (format[i] == '%') {
+            i++;
+            switch (format[i]) {
+                case 's': terminal_putstr(va_arg(args, char *)); break;
+                case 'c': terminal_putchar((char)va_arg(args, int)); break;
+                case 'x': 
+                    terminal_putstr("0x"); 
+                    terminal_putnbr_base(va_arg(args, uint32_t), 16); 
+                    break;
+                case 'd': {
+                    int d = va_arg(args, int);
+                    if (d < 0) { terminal_putchar('-'); d = -d; }
+                    terminal_putnbr_base((uint32_t)d, 10);
+                    break;
+                }
+                case '%': terminal_putchar('%'); break;
+            }
+        } else {
+            terminal_putchar(format[i]);
+        }
+    }
+    va_end(args);
+}
+
+void terminal_putnbr_base(uint32_t n, int base) {
+    char *digits = "0123456789abcdef";
+    if (n >= (uint32_t)base)
+        terminal_putnbr_base(n / base, base);
+    terminal_putchar(digits[n % base]);
+}
+
+size_t strlen(const char* str) {
+    size_t len = 0;
+    while (str[len]) len++;
+    return len;
+}
+
+
+
