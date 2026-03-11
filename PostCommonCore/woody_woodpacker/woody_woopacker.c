baa22f085657af35ad53061b8b05a1a2f259d4b6
Adrien Hors <ahors@f6r2s15.clusters.42paris.fr>
2026-03-11 15:02:44 +0100

    Woody_woodpacker - Base for checking elf64 file is done

---
+++ PostCommonCore/woody_woodpacker/woody_woopacker.c
@@ -0,0 +1,53 @@
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <elf.h>
+#include <stdlib.h>
+
+int check_elf(Elf64_Ehdr *header)
+{
+    if (header->e_ident[EI_MAG0] != ELFMAG0 ||
+        header->e_ident[EI_MAG1] != ELFMAG1 ||
+        header->e_ident[EI_MAG2] != ELFMAG2 ||
+        header->e_ident[EI_MAG3] != ELFMAG3)
+        {
+            return (1); 
+            exit(EXIT_FAILURE);
+        }
+    return (0);
+}
+
+
+int main(int ac, char **av)
+{
+    if (ac != 2) 
+    {
+        printf("Incorrect number of arguments\n");
+        return (1);
+    }
+
+    int fd = open(av[1], O_RDONLY);
+    if (fd < 0) return (1);
+
+
+    struct stat st; // Define in sys/stat (holds the meta data of a file)
+    fstat(fd, &st); // Fills st with file info from read file
+
+    void *ptr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0); // Links memory zone directly to file content
+    if (ptr == MAP_FAILED) return  (1);
+
+    Elf64_Ehdr *header = (Elf64_Ehdr *)ptr;
+    fstat(fd, &st);
+
+    if(check_elf(header) != 1)
+        printf("It is a ELF64 file\n");
+    else 
+        printf("It is not a ELF64 file\n");
+        
+
+    munmap(ptr,st.st_size); // Free of mmap
+    close(fd);
+    return(0);
+}
