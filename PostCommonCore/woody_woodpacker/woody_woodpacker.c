218829f887c07112312cb7f513b893704f139824
Adrien Hors <ahors@f6r2s15.clusters.42paris.fr>
2026-03-11 15:37:33 +0100

    Woody-Woodpacker - start (found if elf64 file and load segment)

--- PostCommonCore/woody_woodpacker/woody_woodpacker.c
+++ PostCommonCore/woody_woodpacker/woody_woodpacker.c
@@ -20,6 +20,37 @@ int check_elf(Elf64_Ehdr *header)
 }
 
 
+int findLoadHeader(Elf64_Ehdr *header)
+{
+    int num_segments = header->e_phnum;
+
+    Elf64_Phdr *phdr = (Elf64_Phdr *)((char *)header + header->e_phoff);
+
+    printf("Number of segments: %d\n", num_segments);
+
+    for (int i = 0; i < num_segments; i++)
+    {
+        // Look for segment of type LOAD (PT_LOAD = 1)
+        if (phdr[i].p_type == PT_LOAD)
+        {
+            // And executable (PF_X = 1)
+            if (phdr[i].p_flags & PF_X)
+            {
+                printf("Segment LOAD Executable found (index %d) :\n", i);
+                printf("  Offset file : 0x%lx\n", phdr[i].p_offset);
+                printf("  Address Virtual : 0x%lx\n", phdr[i].p_vaddr);
+                printf("  Size in the file: %ld bytes\n", phdr[i].p_filesz);
+                
+                // C'est ici que tu peux calculer la "cave"
+                // cave_start = phdr[i].p_offset + phdr[i].p_filesz;
+            }
+        }
+    }
+    return (0);
+
+}
+
+
 int main(int ac, char **av)
 {
     if (ac != 2) 
@@ -39,13 +70,15 @@ int main(int ac, char **av)
     if (ptr == MAP_FAILED) return  (1);
 
     Elf64_Ehdr *header = (Elf64_Ehdr *)ptr;
-    fstat(fd, &st);
+    findLoadHeader(header);
 
     if(check_elf(header) != 1)
         printf("It is a ELF64 file\n");
     else 
+    {
         printf("It is not a ELF64 file\n");
-        
+        exit (EXIT_FAILURE);
+    }    
 
     munmap(ptr,st.st_size); // Free of mmap
     close(fd);
